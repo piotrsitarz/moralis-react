2d80c9d095c3e56b163c1f049bfc15b54879f222
Piotr Sitarz <piotrsitarz@onet.eu>
2022-02-15 22:08:48 +0100

    feat: init handle moralis auth and logout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useMoralis } from "react-moralis";
 
 function App() {
+  const { authenticate, logout, isAuthenticated, user } = useMoralis();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={authenticate}>Authenticate</button>
+        <button onClick={logout}>Logout</button>
+        <button onClick={() => console.log('user', user)}>Check User</button>
+        {isAuthenticated ? 'auth' : 'notAuth'}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <MoralisProvider appId="" serverUrl="">
     <App />
-  </React.StrictMode>,
+  </MoralisProvider>,
   document.getElementById('root')
 );
 
